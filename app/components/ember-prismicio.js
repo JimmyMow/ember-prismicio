ce356988af0e739e9fec1506d50fa0bc2d7e8ac4
Jack Mallers <jimmymowschess@gmail.com>
2015-07-23 18:04:24 -0500

    working prototype. gonna change structure

--- app/components/ember-prismicio.js
+++ app/components/ember-prismicio.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: 'div',
+  classNames: ['ember-prismicio', 'title-list'],
   apiEndPoint: 'https://jackschessblog.prismic.io/api',
   content: null,
 
